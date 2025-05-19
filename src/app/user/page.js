e4c79638fd236ecb4bfb3bb4c5f14fa560279e48
aztrix-codes <khanarman9113@gmail.com>
2025-05-19 16:44:41 +0530

    minor changes

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,37 +1,31 @@
 'use client'
 import React from "react";
+import { Briefcase, PlusCircle, CreditCard, Clock, FileText, Users, MessageCircle, Info } from "lucide-react";
 import "./userPage.css";
 
 function Page() {
-
+  const menuItems = [
+    { title: "Add new share", icon: <PlusCircle className="tile-icon" /> },
+    { title: "My shares", icon: <Briefcase className="tile-icon" /> },
+    { title: "Total shares", icon: <Users className="tile-icon" /> },
+    { title: "Total paid", icon: <CreditCard className="tile-icon" /> },
+    { title: "Total pending", icon: <Clock className="tile-icon" /> },
+    { title: "My invoice", icon: <FileText className="tile-icon" /> },
+    { title: "About us", icon: <Info className="tile-icon" /> },
+    { title: "Contact us", icon: <MessageCircle className="tile-icon" /> }
+  ];
 
   return (
     <div className="page-container">
+      {/* <h1 className="page-title">Dashboard</h1> */}
+      
       <div className="tiles-grid">
-          <div  className="tile">
-            <h3 className="tile-title">Add new share</h3>
+        {menuItems.map((item, index) => (
+          <div key={index} className="tile">
+            {item.icon}
+            <h3 className="tile-title">{item.title}</h3>
           </div>
-          <div  className="tile">
-            <h3 className="tile-title">My shares</h3>
-          </div>
-          <div  className="tile">
-            <h3 className="tile-title">Total shares</h3>
-          </div>
-          <div  className="tile">
-            <h3 className="tile-title">Total paid</h3>
-          </div>
-          <div  className="tile">
-            <h3 className="tile-title">Total pending</h3>
-          </div>
-          <div  className="tile">
-            <h3 className="tile-title">My invoice</h3>
-          </div>
-          <div  className="tile">
-            <h3 className="tile-title">About us</h3>
-          </div>
-          <div  className="tile">
-            <h3 className="tile-title">Contact us</h3>
-          </div>
+        ))}
       </div>
     </div>
   );
