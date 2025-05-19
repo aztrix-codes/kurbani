f4dc9b0600fd713b8ee8088423e52a305b8c45bf
aztrix-codes <khanarman9113@gmail.com>
2025-05-19 16:57:43 +0530

    routing set in user

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,30 +1,34 @@
 'use client'
 import React from "react";
+import Link from "next/link";
 import { Briefcase, PlusCircle, CreditCard, Clock, FileText, Users, MessageCircle, Info } from "lucide-react";
 import "./userPage.css";
 
 function Page() {
   const menuItems = [
-    { title: "Add new share", icon: <PlusCircle className="tile-icon" /> },
-    { title: "My shares", icon: <Briefcase className="tile-icon" /> },
-    { title: "Total shares", icon: <Users className="tile-icon" /> },
-    { title: "Total paid", icon: <CreditCard className="tile-icon" /> },
-    { title: "Total pending", icon: <Clock className="tile-icon" /> },
-    { title: "My invoice", icon: <FileText className="tile-icon" /> },
-    { title: "About us", icon: <Info className="tile-icon" /> },
-    { title: "Contact us", icon: <MessageCircle className="tile-icon" /> }
+    { title: "Add new share", icon: <PlusCircle className="tile-icon" />, href: "/user/new-share" },
+    { title: "My shares", icon: <Briefcase className="tile-icon" />, href: "/user/my-shares" },
+    { title: "Total shares", icon: <Users className="tile-icon" />, href: "#" },
+    { title: "Total paid", icon: <CreditCard className="tile-icon" />, href: "#" },
+    { title: "Total pending", icon: <Clock className="tile-icon" />, href: "#" },
+    { title: "My invoice", icon: <FileText className="tile-icon" />, href: "#" },
+    { title: "About us", icon: <Info className="tile-icon" />, href: "#" },
+    { title: "Contact us", icon: <MessageCircle className="tile-icon" />, href: "/user/contact-us" }
   ];
 
   return (
     <div className="page-container">
+      {/* Decorative elements are handled in CSS */}
       {/* <h1 className="page-title">Dashboard</h1> */}
       
       <div className="tiles-grid">
         {menuItems.map((item, index) => (
-          <div key={index} className="tile">
-            {item.icon}
-            <h3 className="tile-title">{item.title}</h3>
-          </div>
+          <Link key={index} href={item.href} className="tile-link">
+            <div className="tile">
+              {item.icon}
+              <h3 className="tile-title">{item.title}</h3>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
